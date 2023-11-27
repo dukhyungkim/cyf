513987615fba5483b09cb549ce10e81744e07755
DukHyung Kim <idealismboy@gmail.com>
2023-11-27 23:33:22 +0900

    add -m option

--- 02.cli-files/go/go-ls/cmd/root.go
+++ 02.cli-files/go/go-ls/cmd/root.go
@@ -3,17 +3,25 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
 
 func Execute() {
-	readPath := "."
-	if len(os.Args) > 1 {
-		readPath = os.Args[1]
-	}
-
-	if readPath == "-h" {
+	if slices.Contains(os.Args, "-h") {
 		printHelp(os.Args[0])
 		return
+	}
+
+	readPath := "."
+	var opts []string
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		if strings.HasPrefix(arg, "-") {
+			opts = append(opts, arg)
+			continue
+		}
+		readPath = arg
 	}
 
 	stat, err := os.Stat(readPath)
@@ -26,17 +34,28 @@ func Execute() {
 		fmt.Println(readPath)
 		return
 	}
-	printDir(err, readPath)
+	printDir(readPath, opts...)
 }
 
-func printDir(err error, readPath string) {
+func printDir(readPath string, opts ...string) {
 	entries, err := os.ReadDir(readPath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, entry := range entries {
-		fmt.Println(entry.Name())
+	if len(opts) == 0 {
+		for _, entry := range entries {
+			fmt.Println(entry.Name())
+		}
+		return
+	}
+	switch opts[0] {
+	case "-m":
+		names := make([]string, len(entries))
+		for i, entry := range entries {
+			names[i] = entry.Name()
+		}
+		fmt.Println(strings.Join(names, ", "))
 	}
 }
 
@@ -44,4 +63,5 @@ func printHelp(name string) {
 	fmt.Printf("usage: %s [file ...]\n", name)
 	fmt.Println("  Options:")
 	fmt.Println("  -h\t\tprint this message")
+	fmt.Println("  -m\t\tStream output format; list files across the page, separated by commas.")
 }
