3aa0cd812e6f833bc78f3e32961949445547b4c6
DukHyung Kim <idealismboy@gmail.com>
2023-12-04 23:21:31 +0900

    add -m option

--- 02.cli-files/rust/rs-ls/src/main.rs
+++ 02.cli-files/rust/rs-ls/src/main.rs
@@ -2,27 +2,56 @@ use std::{env, fs, io};
 use std::path::Path;
 
 fn main() {
-    let args : Vec<_> = env::args().collect();
+    let args: Vec<_> = env::args().collect();
     if args.contains(&String::from("-h")) {
         print_help(&args[0]);
         return;
     }
 
-    let dir = Path::new(&args[1]);
+    let mut read_path = ".";
+    let mut opts = Vec::new();
+    for arg in &args[1..] {
+        if arg.starts_with("-") {
+            opts.push(arg.as_str());
+            continue;
+        }
+        read_path = arg
+    }
+
+    let dir = Path::new(read_path);
     if !dir.is_dir() {
         println!("{}", dir.to_str().unwrap());
         return;
     }
-    print_dir(dir).unwrap();
+    let entries = read_dir(dir).unwrap();
+    print_dir(entries, opts);
 }
 
-fn print_dir(dir :&Path) -> io::Result<()> {
+fn read_dir(dir: &Path) -> io::Result<Vec<String>> {
+    let mut entries = Vec::new();
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let name = entry.file_name();
-        println!("{}", name.to_str().unwrap());
+        let filename = name.to_str().unwrap().to_string();
+        entries.push(filename);
     }
-    Ok(())
+
+    entries.sort();
+    Ok(entries)
+}
+
+fn print_dir(entries: Vec<String>, opts: Vec<&str>) {
+    if opts.is_empty() {
+        entries.iter().for_each(|name| println!("{}", name));
+        return;
+    }
+
+    if opts[0] == "-m" {
+        println!("{}", entries.join(", "));
+        return;
+    }
+
+    entries.iter().for_each(|name| println!("{}", name));
 }
 
 fn print_help(name: &str) {
