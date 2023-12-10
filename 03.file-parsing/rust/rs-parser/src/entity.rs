b91b0203f26e0f7f50b0cd34f029181ab82d7782
DukHyung Kim <idealismboy@gmail.com>
2023-12-10 23:45:27 +0900

    implement ord for correct min/max

--- 03.file-parsing/rust/rs-parser/src/entity.rs
+++ 03.file-parsing/rust/rs-parser/src/entity.rs
@@ -1,4 +1,6 @@
-#[derive(Clone, PartialOrd, PartialEq, Ord, Eq)]
+use std::cmp::Ordering;
+
+#[derive(Debug, Clone)]
 pub struct NameScore {
     pub name: String,
     pub high_score: i32,
@@ -7,5 +9,25 @@ pub struct NameScore {
 impl NameScore {
     pub fn new(name: String, high_score: i32) -> Self {
         Self { name, high_score }
+    }
+}
+
+impl Eq for NameScore {}
+
+impl PartialEq<Self> for NameScore {
+    fn eq(&self, other: &Self) -> bool {
+        self.high_score == other.high_score
+    }
+}
+
+impl PartialOrd<Self> for NameScore {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for NameScore {
+    fn cmp(&self, other: &Self) -> Ordering {
+        return self.high_score.cmp(&other.high_score);
     }
 }
