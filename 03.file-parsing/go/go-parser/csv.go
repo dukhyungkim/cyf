d04eb65b72a923925523bb9d1d2bebd9b8dcecc6
DukHyung Kim <idealismboy@gmail.com>
2023-12-11 23:32:13 +0900

    more readability

--- 03.file-parsing/go/go-parser/csv.go
+++ 03.file-parsing/go/go-parser/csv.go
@@ -6,19 +6,26 @@ import (
 )
 
 func parseCSV(file []byte) ([]NameScore, error) {
+	const NameIndex = 0
+	const ScoreIndex = 1
+	const MinColumnCount = 2
+
 	contents := string(file)
 	rows := strings.Split(contents, "\n")
 
-	dataLen := len(rows) - 2
-	nameScores := make([]NameScore, dataLen)
-	for i, row := range rows[1:] {
+	dataLen := len(rows) - 2 // wπith Header
+	nameScores := make([]NameScore, 0, dataLen)
+	for _, row := range rows[1:] {
 		columns := strings.Split(row, ",")
-		if len(columns) < 2 {
+		if len(columns) < MinColumnCount {
 			continue
 		}
 
-		score, _ := strconv.Atoi(columns[1])
-		nameScores[i] = NewScore(columns[0], int32(score))
+		name := columns[NameIndex]
+		score, _ := strconv.Atoi(columns[ScoreIndex])
+
+		ns := NewScore(name, int32(score))
+		nameScores = append(nameScores, ns)
 	}
 	return nameScores, nil
 }
