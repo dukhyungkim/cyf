1207ecc3c3191cc6aa705825dc4ebb205b158ddb
DukHyung Kim <idealismboy@gmail.com>
2024-01-13 23:53:57 +0900

    use From trait instead of Into

--- 05.server-database/rs-server-db/src/dto.rs
+++ 05.server-database/rs-server-db/src/dto.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 use crate::entity;
-use crate::entity::NewImage;
 
 #[derive(Debug, Deserialize)]
 pub struct ImageRequest {
@@ -21,16 +20,6 @@ impl From<entity::Image> for Image {
             title: value.title,
             url: value.url,
             alt_text: value.alt_text.unwrap_or("".to_string()),
-        }
-    }
-}
-
-impl Into<entity::NewImage> for Image {
-    fn into(self) -> NewImage {
-        NewImage{
-            title: self.title,
-            url: self.url,
-            alt_text: Some(self.alt_text),
         }
     }
 }
--- 05.server-database/rs-server-db/src/entity.rs
+++ 05.server-database/rs-server-db/src/entity.rs
@@ -1,4 +1,5 @@
 use diesel::{Insertable, Queryable};
+use crate::dto;
 
 #[derive(Debug, Clone, Queryable)]
 pub struct Image {
@@ -14,4 +15,14 @@ pub struct NewImage {
     pub title: String,
     pub url: String,
     pub alt_text: Option<String>,
+}
+
+impl From<dto::Image> for NewImage {
+    fn from(value: dto::Image) -> Self {
+        Self {
+            title: value.title,
+            url: value.url,
+            alt_text: Some(value.alt_text),
+        }
+    }
 }
